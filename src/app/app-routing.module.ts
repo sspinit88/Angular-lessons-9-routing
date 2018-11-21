6651983be47bd2731598fc02ebc40706ebe85d87
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-21 13:38:00 +0300

    init AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CarsPageComponent} from "./cars-page/cars-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
-const routes: Routes = [];
+
+// создаем массив, отвечающий за роуты
+// каждый из элементов массива должен содержать два обязателных поля:
+// 1)  path - отвечает за роут в адресной стоке
+// 2) component - компонент, который будет загружаться при обращению к роуту
+
+// важно импортироваь RouterModule, затем зарегистрировать его в imports
+// передаем RouterModule метод .forRoot(), в метод передаем массив с роутами -> RouterModule.forRoot(appRoutes)
+
+const appRoutes: Routes = [
+    // тут path равен пустой строке, так как, когда будем заходить на localhost,
+    // будет отображаться определенны компонент
+
+    {
+        path: '',
+        component: HomePageComponent
+    },
+    // в адресной строке будет путь: localhost:4200/cars
+    {
+        path: 'cars',
+        component: CarsPageComponent
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // указываем входные и выходные данные модуля
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
+
+// импортируем в app.module.ts
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,32 +7,9 @@ import {HomePageComponent} from './home-page/home-page.component';
 import {CarsPageComponent} from './cars-page/cars-page.component';
 import {CarsService} from './cars.service';
 import {HttpClientModule} from "@angular/common/http";
-import {Routes, RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 
-// создаем массив, отвечающий за роуты
-// каждый из элементов массива должен содержать два обязателных поля:
-// 1)  path - отвечает за роут в адресной стоке
-// 2) component - компонент, который будет загружаться при обращению к роуту
-
-// важно импортироваь RouterModule, затем зарегистрировать его в imports
-// передаем RouterModule метод .forRoot(), в метод передаем массив с роутами -> RouterModule.forRoot(appRoutes)
-
-
-
-const appRoutes: Routes = [
-    // тут path равен пустой строке, так как, когда будем заходить на localhost,
-    // будет отображаться определенны компонент
-
-    {
-        path: '',
-        component: HomePageComponent
-    },
-    // в адресной строке будет путь: localhost:4200/cars
-    {
-        path: 'cars',
-        component: CarsPageComponent
-    },
-];
+// для всего перечисленного лучше всего создать отдельный модульный файл и там вести работу, что и было сделанно
 
 @NgModule({
     declarations: [
@@ -44,7 +21,8 @@ const appRoutes: Routes = [
         BrowserModule,
         FormsModule,
         HttpClientModule,
-        RouterModule.forRoot(appRoutes)
+        // импортируем
+        AppRoutingModule
     ],
     providers: [CarsService],
     bootstrap: [AppComponent]
