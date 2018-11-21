e191224ad795c3c80f5e50c4b80882f11a9d2f22
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-21 17:15:01 +0300

    fix bug html app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: [` 
-        .navbar-toggler.active {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styles: [`
+        li.active {
             border: 1px solid red;
         }
-  `]
+
+        a {
+            font-size: 20px;
+        }
+    `]
 })
 export class AppComponent {
 
