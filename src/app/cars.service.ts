10b3c069ea6b9a46df64ebcf99dc4247d4a73ee5
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-21 16:07:06 +0300

    fin

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -5,19 +5,27 @@ export class CarsService {
   cars = [
     {
       name: 'Ford',
-      id: 1
+      id: 1,
+      year: 2010,
+      color: 'red'
     },
     {
       name: 'Audi',
-      id: 2
+      id: 2,
+      year: 2001,
+      color: 'pink'
     },
     {
       name: 'BMW',
-      id: 3
+      id: 3,
+      year: 1985,
+      color: 'grey'
     },
     {
       name: 'Mercedes',
-      id: 4
+      id: 4,
+      year: 1956,
+      color: 'green'
     }
   ]
 }
--- src/app/cat-page/cat-page.component.ts
+++ src/app/cat-page/cat-page.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
     selector: 'app-cat-page',
@@ -10,8 +10,11 @@ export class CatPageComponent implements OnInit {
 
     id: string;
     name: string;
+    color: string;
+    year: string;
+    hash: string;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private router: Router) {
     }
 
     ngOnInit() {
@@ -21,15 +24,36 @@ export class CatPageComponent implements OnInit {
         this.id = this.route.snapshot.params['id'];
         this.name = this.route.snapshot.params['name'];
 
-        // все в/у параметры можно получать и иным способом
+        this.color = this.route.snapshot.queryParams['color'];  // (s2)
+        this.year = this.route.snapshot.queryParams['year'];  // (s2)
+
+        this.hash = this.route.snapshot.fragment; // (s3)
+
+        // все в/у параметры можно получать и иным способом (s1)
         // это уже стрим, подписываемся на прослушку изменения адресной строки
-        this.route.params.subscribe((params:Params) => {
+        this.route.params.subscribe((params: Params) => {
             console.log(params); // получим объект с полем id и name
             this.id = params['id'];
             this.name = params['name'];
         });
 
+        // - (s4) - подписываемся на динамическое обновление параметров
+        this.route.params.subscribe((params: Params) => {
+            this.color = params['color'];
+            this.year = params['year'];
 
+        });
+    }
+
+    // для осуществления перехода по странице необходимо подключить "private router: Router"
+    openPage() {
+        this.router.navigate(['./cars', 8, 'testAuto'], {
+            queryParams: {
+                color: 'pink',
+                year: 1995
+            },
+            fragment: 'pic'
+        });
     }
 
 }
