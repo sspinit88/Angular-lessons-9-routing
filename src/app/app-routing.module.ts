131de970e3a9240020faeeb5d9992f61ae36f1ca
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-21 15:23:36 +0300

    fin first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,36 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CarsPageComponent} from "./cars-page/cars-page.component";
 import {HomePageComponent} from "./home-page/home-page.component";
-
-
-// создаем массив, отвечающий за роуты
-// каждый из элементов массива должен содержать два обязателных поля:
-// 1)  path - отвечает за роут в адресной стоке
-// 2) component - компонент, который будет загружаться при обращению к роуту
-
-// важно импортироваь RouterModule, затем зарегистрировать его в imports
-// передаем RouterModule метод .forRoot(), в метод передаем массив с роутами -> RouterModule.forRoot(appRoutes)
+import {CatPageComponent} from "./cat-page/cat-page.component";
 
 const appRoutes: Routes = [
-    // тут path равен пустой строке, так как, когда будем заходить на localhost,
-    // будет отображаться определенны компонент
-
     {
         path: '',
         component: HomePageComponent
     },
-    // в адресной строке будет путь: localhost:4200/cars
     {
         path: 'cars',
         component: CarsPageComponent
     },
+    // запись  path: 'cars/:id' (вместо id любую переменную ставь)
+    // это для перехода на внутренние страницы каждой из машин (динамически)
+    // далее смотри cars-page.component.html
+
+    // теперь зададим нов динам переменную
+    {
+        path: 'cars/:id/:name',
+        component: CatPageComponent
+    },
 ];
 
 @NgModule({
-  // указываем входные и выходные данные модуля
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 
-// импортируем в app.module.ts
 export class AppRoutingModule { }
--- src/app/cat-page/cat-page.component.ts
+++ src/app/cat-page/cat-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
-  selector: 'app-cat-page',
-  templateUrl: './cat-page.component.html',
-  styleUrls: ['./cat-page.component.css']
+    selector: 'app-cat-page',
+    templateUrl: './cat-page.component.html',
+    styleUrls: ['./cat-page.component.css']
 })
 export class CatPageComponent implements OnInit {
 
-  constructor() { }
+    id: string;
+    name: string;
 
-  ngOnInit() {
-  }
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        // snapshot - позволяет обратиться к адресной строке
+        // params - объект, где хранятся все эл, прописанные в адресной строке
+
+        this.id = this.route.snapshot.params['id'];
+        this.name = this.route.snapshot.params['name'];
+
+        // все в/у параметры можно получать и иным способом
+        // это уже стрим, подписываемся на прослушку изменения адресной строки
+        this.route.params.subscribe((params:Params) => {
+            console.log(params); // получим объект с полем id и name
+            this.id = params['id'];
+            this.name = params['name'];
+        });
+
+
+    }
 
 }
